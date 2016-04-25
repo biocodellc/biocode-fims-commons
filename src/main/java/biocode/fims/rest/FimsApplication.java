6dbda29080e8ed4a8351f2ad3e7c50e247e51ea6
rjewing <ewing.rj@gmail.com>
2016-04-25 16:42:01 -0700

    added Jackson for JSON serialization/deserialization

--- src/main/java/biocode/fims/rest/FimsApplication.java
+++ src/main/java/biocode/fims/rest/FimsApplication.java
@@ -3,6 +3,7 @@ package biocode.fims.rest;
 import biocode.fims.rest.filters.AdminFilter;
 import biocode.fims.rest.filters.AuthenticatedFilter;
 import biocode.fims.settings.SettingsManager;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +17,10 @@ public class FimsApplication extends ResourceConfig {
 
     public FimsApplication() {
         super();
+
+        register(ObjectMapperContextResolver.class);
+        register(JacksonFeature.class);
+
         register(FimsExceptionMapper.class);
 
         register(RequestContextFilter.class);
---
+++ src/main/java/biocode/fims/rest/SpringObjectMapper.java
@@ -0,0 +1,19 @@
+package biocode.fims.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.stereotype.Component;
+
+/**
+ * Jackson ObjectMapper with hibernate support
+ */
+@Component
+public class SpringObjectMapper extends ObjectMapper {
+
+    public SpringObjectMapper() {
+        this.registerModule(new Hibernate5Module());
+        this.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+}
