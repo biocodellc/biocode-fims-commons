50587582adfdc1376c2e9fdbb1c5b535db882d36
rjewing <ewing.rj@gmail.com>
2017-02-22 07:56:42 -0800

    remove unused code from CharacterCleaner

--- src/main/java/biocode/fims/reader/CharacterCleaner.java
+++ src/main/java/biocode/fims/reader/CharacterCleaner.java
@@ -18,7 +18,6 @@ public class CharacterCleaner {
 
     public static void main(String[] args) throws IOException {
 
-        //File f = new File("/Users/jdeck/Google Drive/!DIPnet_DB/Repository/1-cleaned_QC2_mdfasta_files/mdfastaQC2_Eucmet_CO1_HL.txt");
         File f = new File("/Users/jdeck/IdeaProjects/biocode-fims/sampledata/test");
 
         FileInputStream fis = new FileInputStream(f);
