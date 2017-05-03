3edada1834e0be323755ccffb162c6681cf81935
rjewing <ewing.rj@gmail.com>
2017-05-03 09:20:55 -0700

    made CSVReader only implement the DataReader interface, removing unnecessary code

--- src/main/java/biocode/fims/reader/plugins/CSVReader.java
+++ src/main/java/biocode/fims/reader/plugins/CSVReader.java
@@ -10,7 +10,6 @@ import biocode.fims.fimsExceptions.errorCodes.FileCode;
 import biocode.fims.models.records.Record;
 import biocode.fims.models.records.RecordMetadata;
 import biocode.fims.models.records.RecordSet;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.util.Assert;
 
 import java.io.*;
@@ -37,7 +36,7 @@ import java.util.stream.Collectors;
  * (1 byte) characters, and that characters 0-8 and 10-31 are all treated as
  * empty whitespace and ignored (unless they occur within a quoted string).
  */
-public class CSVReader implements TabularDataReader {
+public class CSVReader implements DataReader {
     public static final String SHEET_NAME_KEY = "sheetName";
 
     private static final List<String> EXTS = Collections.singletonList("csv");
@@ -52,23 +51,12 @@ public class CSVReader implements TabularDataReader {
 
     private Map<String, List<Record>> entityRecords;
     private List<Entity> recordEntites;
-
-    private LinkedList<String> reclist;
-    //private int currtable = 1;
-
-    // A reference to the file that opened this reader
-    protected String fileName;
-
-    // The number of rows in the active worksheet
-    private int numRows;
-
-    List<String> colNames;
+    private List<String> colNames;
 
     /**
      * This is only to be used for passing the class into the DataReaderFactory
      */
     public CSVReader() {
-        reclist = new LinkedList<String>();
     }
 
     /**
@@ -84,7 +72,6 @@ public class CSVReader implements TabularDataReader {
         this.mapping = mapping;
         this.recordMetadata = recordMetadata;
         this.entityRecords = new HashMap<>();
-        this.reclist = new LinkedList<>();
 
         if (!recordMetadata.has(SHEET_NAME_KEY)) {
             throw new FimsRuntimeException(DataReaderCode.MISSING_METADATA, 500);
@@ -93,6 +80,15 @@ public class CSVReader implements TabularDataReader {
         this.sheetName = String.valueOf(recordMetadata.remove(SHEET_NAME_KEY));
     }
 
+    @Override
+    public boolean handlesExtension(String ext) {
+        return EXTS.contains(ext.toLowerCase());
+    }
+
+    @Override
+    public DataReader newInstance(File file, Mapping mapping, RecordMetadata recordMetadata) {
+        return new CSVReader(file, mapping, recordMetadata);
+    }
 
     @Override
     public List<RecordSet> getRecordSets() {
@@ -103,9 +99,34 @@ public class CSVReader implements TabularDataReader {
         return generateRecordSets();
     }
 
+    private void init() {
+        try {
+            st = new StreamTokenizer(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            throw new FimsRuntimeException(FileCode.READ_ERROR, 500);
+        }
+
+        st.resetSyntax();
+        st.eolIsSignificant(true);
+        st.whitespaceChars(0, 31);
+        st.wordChars(' ', 255);
+        st.wordChars('\t', '\t');
+        st.quoteChar('"');
+        st.ordinaryChar(',');
+
+        setHasNext();
+
+        // Get the first row to populate Column Names
+        colNames = nextRow();
+
+        recordEntites = mapping.getEntities().stream()
+                .filter(e -> sheetName.equals(e.getWorksheet()))
+                .collect(Collectors.toList());
+    }
+
     private void instantiateRecords() {
-        while (tableHasNextRow()) {
-            String[] row = tableGetNextRow();
+        while (hasNext) {
+            LinkedList<String> row = nextRow();
 
             for (Entity e : recordEntites) {
                 try {
@@ -113,7 +134,9 @@ public class CSVReader implements TabularDataReader {
 
                     for (Attribute a : e.getAttributes()) {
                         if (colNames.contains(a.getColumn())) {
-                            String val = row[colNames.indexOf(a.getColumn())];
+                            String val = row.get(
+                                    colNames.indexOf(a.getColumn())
+                            );
 
                             r.set(a.getUri(), val);
                         }
@@ -141,51 +164,12 @@ public class CSVReader implements TabularDataReader {
         return recordSets;
     }
 
-    private void init() {
-        try {
-            st = new StreamTokenizer(new FileReader(file));
-        } catch (FileNotFoundException e) {
-            throw new FimsRuntimeException(FileCode.READ_ERROR, 500);
-        }
-
-        st.resetSyntax();
-        st.eolIsSignificant(true);
-        st.whitespaceChars(0, 31);
-        st.wordChars(' ', 255);
-        st.wordChars('\t', '\t');
-        st.quoteChar('"');
-        st.ordinaryChar(',');
-
-        setHasNext();
-
-        // Get the first row to populate Column Names
-        colNames = Arrays.asList(tableGetNextRow());
-
-        recordEntites = mapping.getEntities().stream()
-                .filter(e -> {
-                    return sheetName.equals(e.getWorksheet());
-                })
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public boolean handlesExtension(String ext) {
-        return EXTS.contains(ext.toLowerCase());
-    }
-
-    @Override
-    public DataReader newInstance(File file, Mapping mapping, RecordMetadata recordMetadata) {
-        return new CSVReader(file, mapping, recordMetadata);
-    }
-
-    @Override
-    public String[] tableGetNextRow() {
-        if (!tableHasNextRow())
+    private LinkedList<String> nextRow() {
+        if (!hasNext)
             throw new NoSuchElementException();
 
         int prevToken = ',';
-        int fieldcnt = 0;
-        reclist.clear();
+        LinkedList<String> row = new LinkedList<>();
 
         try {
             while (st.ttype != StreamTokenizer.TT_EOL &&
@@ -193,17 +177,15 @@ public class CSVReader implements TabularDataReader {
                 if (st.ttype == ',') {
                     // See if we just passed an empty field.
                     if (prevToken == ',') {
-                        reclist.add("");
-                        fieldcnt++;
+                        row.add("");
                     }
                 } else {
                     // See if we just passed an escaped double quote inside
                     // of a quoted string.
                     if (prevToken != ',')
-                        reclist.add(reclist.removeLast() + "\"" + st.sval);
+                        row.add(row.removeLast() + "\"" + st.sval);
                     else {
-                        reclist.add(st.sval);
-                        fieldcnt++;
+                        row.add(st.sval);
                     }
                 }
 
@@ -215,72 +197,12 @@ public class CSVReader implements TabularDataReader {
 
         // Test for the special case of a blank last field.
         if (prevToken == ',') {
-            reclist.add("");
-            fieldcnt++;
+            row.add("");
         }
 
         setHasNext();
 
-        String[] ret = new String[fieldcnt];
-        for (int cnt = 0; cnt < fieldcnt; cnt++)
-            ret[cnt] = reclist.remove();
-
-        return ret;
-    }
-
-    /**
-     * See if the specified file is a CSV file.  Since no "magic number" can
-     * be defined for CSV files, this test is limited to seeing if the file
-     * extension is "csv".  This method also tests if the file actually exists.
-     *
-     * @param filepath The file to test.
-     * @return True if the specified file exists and appears to be a CSV file,
-     * false otherwise.
-     */
-    @Override
-    public boolean testFile(String filepath) {
-        // test if the file exists
-        File file = new File(filepath);
-        if (!file.exists())
-            return false;
-
-        int index = filepath.lastIndexOf('.');
-
-        if (index != -1 && index != (filepath.length() - 1)) {
-            // get the extension
-            String ext = filepath.substring(index + 1);
-
-            if (ext.equals("csv"))
-                return true;
-        }
-
-        return false;
-    }
-
-
-    public boolean openFile(String filepath, String defaultSheetName, String outputFolder) {
-        // Set the input file name
-        fileName = filepath;
-
-        try {
-            st = new StreamTokenizer(new FileReader(filepath));
-        } catch (FileNotFoundException e) {
-            return false;
-        }
-
-        st.resetSyntax();
-        st.eolIsSignificant(true);
-        st.whitespaceChars(0, 31);
-        st.wordChars(' ', 255);
-        st.wordChars('\t', '\t');
-        st.quoteChar('"');
-        st.ordinaryChar(',');
-
-        setHasNext();
-
-        // Get the first row to populate Column Names
-        colNames = Arrays.asList(tableGetNextRow());
-        return true;
+        return row;
     }
 
     /**
@@ -303,127 +225,6 @@ public class CSVReader implements TabularDataReader {
         } else {
             hasNext = false;
         }
-    }
-
-    @Override
-    public boolean hasNextTable() {
-        return false;
-    }
-
-    @Override
-    public void moveToNextTable() {
-        throw new NoSuchElementException();
-    }
-
-    @Override
-    public String getCurrentTableName() {
-        return "table1";
-    }
-
-    @Override
-    public boolean tableHasNextRow() {
-        return hasNext;
-    }
-
-
-    @Override
-    public void closeFile() {
-    }
-
-    @Override
-    public List<String> getColNames() {
-        return colNames;
-    }
-
-    @Override
-    public Sheet getSheet() {
-        return null;
-    }
-
-    @Override
-    public int getNumRows() {
-        if (numRows > 0) {
-            return numRows;
-        }
-
-        try {
-            int lines = countLines(fileName);
-            // decrement by one... assumption is that we MUST have a header for FIMS to work.
-            lines--;
-            numRows = lines;
-            return numRows;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return 0;
-        }
-    }
-
-    /**
-     * An efficient method for counting the lines in a file
-     *
-     * @param filename
-     * @return
-     * @throws IOException
-     */
-    public static int countLines(String filename) throws IOException {
-        int count = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
-            while ((bufferedReader.readLine()) != null) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    @Override
-    public File getInputFile() {
-        return null;
-    }
-
-    @Override
-    public String getStringValue(String column, int row) {
-        return null;
-    }
-
-    @Override
-    public String getStringValue(int col, int row) {
-        return null;
-    }
-
-    @Override
-    public Double getDoubleValue(String column, int row) {
-        return null;
-    }
-
-    @Override
-    public Integer getColumnPosition(String colName) {
-        return null;
-    }
-
-    @Override
-    public void setTable(String table) {
-        table = "sheet1";
-
-    }
-
-    @Override
-    public String getFormatString() {
-        return "CSV";
-    }
-
-    @Override
-    public String getShortFormatDesc() {
-        return "CSV";
-    }
-
-    @Override
-    public String getFormatDescription() {
-        return "comma-separated values";
-    }
-
-    @Override
-    public String[] getFileExtensions() {
-        return new String[]{"csv"};
     }
 
 }
