5825fb0fea6fc1b82ea08491ad64fa05f128e206
John Deck <jdeck88@gmail.com>
2017-05-04 14:06:02 -0700

    adding a URI convertor class

--- src/main/java/biocode/fims/digester/Mapping.java
+++ src/main/java/biocode/fims/digester/Mapping.java
@@ -281,6 +281,7 @@ public class Mapping {
      */
     public synchronized void addMappingRules(File configFile) {
         ConvertUtils.register(new EnumConverter(), DataType.class);
+        ConvertUtils.register(new URIConverter(), URI.class);
         Digester d = new Digester();
         d.push(this);
 
