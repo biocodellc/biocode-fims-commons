5b3378d2ad428a07aa925ea32b220bbbcef5b76f
rjewing <ewing.rj@gmail.com>
2017-05-04 10:56:16 -0700

    add a merge method to RecordSet

--- src/main/java/biocode/fims/models/records/RecordSet.java
+++ src/main/java/biocode/fims/models/records/RecordSet.java
@@ -1,7 +1,10 @@
 package biocode.fims.models.records;
 
 
+import biocode.fims.digester.Entity;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -11,6 +14,18 @@ public class RecordSet {
 
     private List<Record> records;
     private String conceptAlias; //TODO should this be the Entity itself?
+    private Entity entity;
+
+    public RecordSet(Entity entity) {
+        this.entity = entity;
+        this.conceptAlias = entity.getConceptAlias();
+        this.records = new ArrayList<>();
+    }
+
+    public RecordSet(Entity entity, List<Record> records) {
+        this(entity);
+        this.records.addAll(records);
+    }
 
     public RecordSet(String conceptAlias) {
         this.conceptAlias = conceptAlias;
@@ -36,5 +51,22 @@ public class RecordSet {
 
     public String conceptAlias() {
         return conceptAlias;
+    }
+
+    public void merge(List<Record> records) {
+
+        for (Record r : records) {
+            if (addRecord(r)) {
+                this.records.add(r);
+            }
+        }
+
+    }
+
+    private boolean addRecord(Record record) {
+        String uniqueKey = entity.getUniqueKey();
+
+        return records.stream()
+                .noneMatch(r -> record.get(uniqueKey).equals(r.get(uniqueKey)));
     }
 }
--- src/test/java/biocode/fims/models/records/RecordSetTest.java
+++ src/test/java/biocode/fims/models/records/RecordSetTest.java
@@ -1,8 +1,11 @@
 package biocode.fims.models.records;
 
+import biocode.fims.digester.Entity;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -14,7 +17,7 @@ public class RecordSetTest {
 
     @Test
     public void is_empty_with_no_records() {
-        RecordSet recordSet = new RecordSet("resource");
+        RecordSet recordSet = new RecordSet(entity());
 
         assertTrue(recordSet.isEmpty());
         assertEquals("resource", recordSet.conceptAlias());
@@ -22,9 +25,8 @@ public class RecordSetTest {
 
     @Test
     public void has_records() {
-        RecordSet recordSet = new RecordSet("resource");
-        GenericRecord r = new GenericRecord();
-        r.set("prop1", "true");
+        RecordSet recordSet = new RecordSet(entity());
+        Record r = record1();
         recordSet.add(r);
 
         assertFalse(recordSet.isEmpty());
@@ -33,15 +35,69 @@ public class RecordSetTest {
 
     @Test
     public void has_records_from_constructor() {
-        List<Record> records = new ArrayList();
-        GenericRecord r = new GenericRecord();
-        r.set("prop1", "true");
-        records.add(r);
+        List<Record> records = Collections.singletonList(record1());
 
-        RecordSet recordSet = new RecordSet("resource", records);
+        RecordSet recordSet = new RecordSet(entity(), records);
 
         assertFalse(recordSet.isEmpty());
         assertEquals(records, recordSet.records());
+    }
+
+    @Test
+    public void should_add_new_records_when_merge() {
+        List<Record> startingRecords = Collections.singletonList(record1());
+
+        RecordSet recordSet = new RecordSet(entity(), startingRecords);
+        recordSet.merge(Collections.singletonList(record2()));
+
+        assertEquals(2, recordSet.records().size());
+
+        String uniqueKey = entity().getUniqueKey();
+
+        List<Record> records = recordSet.records();
+        assertEquals(1,
+                records.stream()
+                        .filter(
+                                r -> r.get(uniqueKey).equals(record1().get(uniqueKey))
+                        ).count()
+        );
+
+        assertEquals(1,
+                records.stream()
+                        .filter(
+                                r -> r.get(uniqueKey).equals(record2().get(uniqueKey))
+                        ).count()
+        );
+    }
+
+    @Test
+    public void should_not_add_duplicate_records_when_merge() {
+        List<Record> startingRecords = Collections.singletonList(record1());
+
+        RecordSet recordSet = new RecordSet(entity(), startingRecords);
+        recordSet.merge(Collections.singletonList(record1()));
+
+        assertEquals(1, recordSet.records().size());
+        assertEquals(startingRecords, recordSet.records());
+    }
+
+    private Record record1() {
+        GenericRecord r = new GenericRecord();
+        r.set("column1", "1");
+        return r;
+    }
+
+    private Record record2() {
+        GenericRecord r = new GenericRecord();
+        r.set("column1", "2");
+        return r;
+    }
+
+    private Entity entity() {
+        Entity entity = new Entity();
+        entity.setConceptAlias("resource");
+        entity.setUniqueKey("column1");
+        return entity;
     }
 
 }
