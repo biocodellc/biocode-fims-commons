efc8cf8a7ef450959d7a2a10d47e126a768dacba
rjewing <ewing.rj@gmail.com>
2017-06-17 17:28:50 -0700

    only allow users to update their own profile

--- src/main/java/biocode/fims/rest/services/rest/subResources/UsersResource.java
+++ src/main/java/biocode/fims/rest/services/rest/subResources/UsersResource.java
@@ -125,9 +125,8 @@ public class UsersResource extends FimsService {
      * update a users profile
      *
      * @param username
-     * @param user
-     * @responseMessage 403 you must be authenticated to update a users profile `biocode.fims.utils.ErrorInfo
-     * @responseMessage 403 you must be a project admin to update another user's profile `biocode.fims.utils.ErrorInfo
+     * @param updatedUser
+     * @responseMessage 403 You cannot update another user's profile. `biocode.fims.utils.ErrorInfo
      */
     @JsonView(Views.Detailed.class)
     @UserEntityGraph("User.withProjects")
@@ -135,23 +134,18 @@ public class UsersResource extends FimsService {
     @PUT
     @Authenticated
     public User updateUser(@PathParam("username") String username,
-                           User user) {
-        if (!userContext.getUser().getUsername().equals(username) &&
-                !userService.isAProjectAdmin(userContext.getUser(), appRoot)) {
-            throw new ForbiddenRequestException("You must be a project admin to access another user's profile");
-        }
-
-        User existingUser = userService.getUser(username);
-        if (existingUser == null) {
-            throw new FimsRuntimeException("user not found", 404);
+                           User updatedUser) {
+        User user = userContext.getUser();
+        if (!user.getUsername().equals(username)) {
+            throw new ForbiddenRequestException("You cannot update another user's profile.");
         }
 
         if (!user.isValid(false)) {
             throw new BadRequestException("firstName, lastName, email, and institution are required");
         }
 
-        existingUser.update(user);
-        return userService.update(existingUser);
+        user.update(updatedUser);
+        return userService.update(user);
     }
 
     /**
