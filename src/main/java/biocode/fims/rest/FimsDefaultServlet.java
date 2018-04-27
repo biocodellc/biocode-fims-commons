aef993456e8f86d416112374c61cd9edda689a92
RJ Ewing <ewing.rj@protonmail.com>
2018-04-27 16:52:15 -0700

    FimsDefaultServlet returns 404 if file not found

--- src/main/java/biocode/fims/rest/FimsDefaultServlet.java
+++ src/main/java/biocode/fims/rest/FimsDefaultServlet.java
@@ -8,32 +8,27 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * This servlet is used with angularJs for overriding the jetty servlet responsible for serving static files.
- * When a request is received, we check to see if the file exists in the filesystem. If not, then we want to
- * defer to angularjs for routing. Otherwise, we will have 404's when directly calling an angularjs page.
+ * This servlet is used when Jersey is registered at the same path as we may need to serve resources under.
+ * For ex. if Jersey is registered a /* and we want to serve /doc/test.json, by default, Jersey will throw
+ * a 404 b/c it can't match the rest service. Instead, we can register the Jersey Application as a filter
+ * and set the <initParam>jersey.config.servlet.filter.forwardOn404</initParam> to true, which will forward
+ * the request to jetty if. The disadvantage is that jetty will return a 204 for the request /doc. This
+ * filter will throw a 404, unless the specific file exists on the server
  *
  */
 public class FimsDefaultServlet extends DefaultServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        // Check to see if the requested file exists. If not, then we need to we need to show index.html and let
-        // angular JS router do the work
-        String redirectRoute = "/index.html";
-
+        // Check to see if the requested file exists. If not, throw a 404
         String absoluteFilePath = getServletContext().getRealPath(request.getPathInfo());
         File file = new File(absoluteFilePath);
 
-        if (!file.exists()) {
-            RequestDispatcher dispatcher = request.getRequestDispatcher(redirectRoute);
-            response.reset();
-
-            dispatcher.forward(request, response);
+        if (!file.exists() || file.isDirectory()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
         super.service(request, response);
     }
-
-
 }
